068306455251145e23243cb42923b5f5c4ae16a0
Jonah Johansen <johansenjonah+git@gmail.com>
2021-01-18 18:21:34 -0800

    add controls left, right, down, rotate

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 20;
 
   //add pixel divs
-  for (let i = 0; i < 400; i++) {
+  for (let i = 0; i < width*20; i++) {
     $('.grid').append('<div></div>');
   }
   for (let i = 0; i < width; i++) {
-    $('.grid').append('<div class="taken"></div>');
+    $('.grid').append('<div class="taken ground"></div>');
   }
 
   //select grid from DOM
@@ -53,10 +53,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   colors = ["tetrominoblue","tetrominored","tetrominogreen","tetrominoorange"]
 
-  let currentPosition = Math.floor(Math.random()*width)
+  let currentPosition = 5 //Math.floor(Math.random()*width)
   let currentRotation = Math.floor(Math.random()*4)
   let currentColor = colors[Math.floor(Math.random()*colors.length)]
-  console.log(currentRotation);
 
 
 
@@ -71,6 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //draw the Tetromino
   function draw () {
+    current = theTetrominoes[random][currentRotation]
     current.forEach(index => {
       squares[currentPosition + index].classList.add(currentColor)
     })
@@ -84,7 +84,21 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   //make the tetromino move down every second
-  timerId = setInterval(moveDown, 100)
+  timerId = setInterval(moveDown, 1000)
+
+  //assign functions to keyCodes
+  function control(e) {
+    if(e.keyCode === 37) {
+      moveLeft()
+    } else if (e.keyCode === 39) {
+      moveRight()
+    } else if (e.keyCode === 40) {
+      moveDown()
+    } else if (e.keyCode === 38) {
+      rotate()
+    }
+  }
+  document.addEventListener('keyup', control);
 
   function moveDown() {
     undraw()
@@ -100,16 +114,50 @@ document.addEventListener('DOMContentLoaded', () => {
       //start a new tetromino falling
       random = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random][currentRotation]
-      currentPosition = Math.floor(Math.random()*width)
       currentColor = colors[Math.floor(Math.random()*3)]
       currentRotation = Math.floor(Math.random()*4)
+      currentPosition = 4 // Math.floor(Math.random()*width)
       draw()
     }
   }
 
   //move the tetromino left or right unless at edge or blocked by .taken
+  function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
 
+    if(!isAtLeftEdge) currentPosition -=1
 
+    if(current.some(index => squares[currentPosition + index].classList.contains("taken"))) {
+      currentPosition +=1
+    }
+    draw()
+  }
+  function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width-1)
+
+    if(!isAtRightEdge) {currentPosition +=1}
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPositon -=1
+    }
+
+    draw()
+  }
+  function rotate() {
+    undraw()
+    currentRotation +=1
+    if (currentRotation === 4) {currentRotation = 0}
+    const isAtEdge = current.some(index => (currentPosition + index) % width === width-1) && current.some(index => (currentPosition + index) % width === 0)
+
+    if(isAtEdge) {currentRotation -=1}
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentRotation -=1
+    }
+    draw()
+  }
 
 
 
